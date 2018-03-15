2ff20598f9dd94aef0c4bc10a6321ec8ef32b13f
Randy Rodriguez <randyro91@hotmail.com>
2018-03-15 15:33:08 +0000

    Create project

---
+++ ttpClient/ttpClient/ttpClientPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace ttpClient
+{
+    public partial class ttpClientPage : ContentPage
+    {
+        public ttpClientPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
