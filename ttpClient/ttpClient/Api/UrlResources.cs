ca67f86de2b682b52a41e61a90121c92dd465586
Randy Rodriguez <randyro91@hotmail.com>
2018-03-15 15:43:52 +0000

    Create url resources

---
+++ ttpClient/ttpClient/Api/UrlResources.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace ttpClient.Api
+{
+    public static class UrlResources
+    {
+        public const string ApiBaseUrl = "http://newhuaweiapstudent-10-58-195-152.ncl.ac.uk:8000";
+
+        public const string RegisterUrl = "registry";
+
+        public const string TransactionsUrl = "transations";
+
+        public const string TransactionConfirmUrlFormat = "transations/{0}/confirm"; //param: Transaction Id 
+    }
+}
